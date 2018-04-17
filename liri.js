a93e7140f84d3eb73e8f5462713946b2d69f4dbf
lwerner27 <lwerner27@gmail.com>
2018-04-17 16:00:20 -0500

    oMDB requests are functional

--- liri.js
+++ liri.js
@@ -12,13 +12,23 @@ let client = new Twitter(keys.Twitter);
 
 // Put Arguments into variables
 let action = process.argv[2]
-let twitterHandle = process.argv[3]
+
+let argTwo = ""
+
+for (let i = 3; i < process.argv.length; i++) {
+    if (i === 3) {
+        argTwo = argTwo + process.argv[i]
+    } else {
+        argTwo = argTwo + "_" + process.argv[i]
+    }
+}
 
 // Check for actions
 if (action === 'getTweets') {
 
+    console.clear()
     let params = {
-        screen_name: twitterHandle,
+        screen_name: argTwo,
         count: 20,
     };
 
@@ -33,8 +43,30 @@ if (action === 'getTweets') {
             console.log(error)
         }
     });
+} else if (action === "getMovie") {
+    console.clear()
+    let queryUrl = `http://www.omdbapi.com/?apikey=trilogy&t=${argTwo}`
+    
+    request(queryUrl, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+            let jsonBody = JSON.parse(body)
+            console.log("")
+            console.log(`Title: ${jsonBody.Title}`)
+            console.log(`Released: ${jsonBody.Released}`)
+            console.log(`iMDB Rating: ${jsonBody.Ratings[0].Value}`)
+            console.log(`Rotten Tomatoes Rating: ${jsonBody.Ratings[1].Value}`)
+            console.log(`Produced In: ${jsonBody.Country}`)
+            console.log(`Language: ${jsonBody.Language}`)
+            console.log(`Plot Summary: ${jsonBody.Plot}`)
+            console.log(`Actors: ${jsonBody.Actors}`)
+
+          }
+    }) 
+
 } else {
     // If user doesn't add recognized arguments
+    console.clear()
     console.log("The argument options are as follows:")
     console.log("")
     console.log("getTweets <desired twitter handle>")
